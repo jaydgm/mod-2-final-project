e1fe8ef2f941a989878b778cacfd3d2cfa850c45
jaydgm <rjayd22@gmail.com>
2023-12-08 11:31:25 -0800

    Create array of object with all POIs

---
+++ script.js
@@ -0,0 +1,27 @@
+const POIs = [
+    {
+        name: 'Rebels Roost',
+        popularity: 'high',
+        medallion: true,
+        vault: true
+    },
+    {
+        name: 'Lavish Lair',
+        popularity: 'high',
+        medallion: true,
+        vault: true
+    },
+    {
+        name: 'Classy Courts',
+        popularity: 'medium',
+        medallion: false,
+        vault: false
+    },
+    {
+        name: 'Ritzy Riviera',
+        ppularity: 'medium',
+        medallion: true,
+        vault: false
+    }
+]
+
